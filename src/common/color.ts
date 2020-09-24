b84484d1516153e2ccce31658f01254470b6185b
SeunghyunWoo <seunghyunwoo99@gmail.com>
2020-09-25 02:15:34 +0900

    전역적으로 사용되는 색상값과 size 설정

---
+++ src/common/color.ts
@@ -0,0 +1,50 @@
+export default {
+  /** 글자 색 */
+  text: {
+    /** #333333 black: ![](https://www.colorhexa.com/333333.png) */
+    primary1: '#333333',
+    /** #000000 blackTwo: ![](https://www.colorhexa.com/000000.png) */
+    primary2: '#000000',
+    /** #666666 brownishGey: ![](https://www.colorhexa.com/666666.png) */
+    secondary1: '#666666',
+    /** #999999 brownGrey: ![](https://www.colorhexa.com/999999.png) */
+    secondary2: '#999999',
+    /** #bbbbbb veryLightPink: ![](https://www.colorhexa.com/bbbbbb.png) */
+    hint: '#bbbbbb',
+    /** #ffffff white: ![](https://www.colorhexa.com/ffffff.png) */
+    white: '#ffffff',
+    /** 앱 메인 어두운 남색 색상 ![](https://www.colorhexa.com/446878.png) */
+    mainDark: '#446878',
+    /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/fef4e8.png) */
+    mainLight: '#fef4e8',
+  },
+
+  /** #eeeeee veryLightPinkTwo: ![](https://www.colorhexa.com/eeeeee.png) */
+  divider: '#eeeeee',
+  /** 버튼 색상 */
+  button: {
+    /** 앱 메인 어두운 남색 색상 ![](https://www.colorhexa.com/446878.png) */
+    mainDark: '#446878',
+    /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/fef4e8.png) */
+    mainLight: '#fef4e8',
+  },
+  /** 배경 색상 */
+  background: {
+    /** 앱 메인 어두운 남색 색상 ![](https://www.colorhexa.com/446878.png) */
+    mainDark: '#446878',
+    /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/fef4e8.png) */
+    mainLight: '#fef4e8',
+    /** #ffffff whiteTwo: ![](https://www.colorhexa.com/ffffff.png) */
+    white: '#ffffff',
+    /** #fafafa iOS 디폴트 컬러: ![](https://www.colorhexa.com/fafafa.png) */
+    default: '#fafafa',
+  },
+
+  /** 기타 색상들 */
+  palette: {
+    /** 앱 메인 어두운 남색 색상 ![](https://www.colorhexa.com/446878.png) */
+    mainDark: '#446878',
+    /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/fef4e8.png) */
+    mainLight: '#fef4e8',
+  },
+}
