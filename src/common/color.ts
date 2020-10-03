dd40e3fac630b7381e4d1fc509bfc7a128ccb61b
SeunghyunWoo <seunghyunwoo99@gmail.com>
2020-10-04 04:43:52 +0900

    복습페이지 임시데이터 추가 및 UI 디자인 변경

--- src/common/color.ts
+++ src/common/color.ts
@@ -27,8 +27,8 @@ export default {
     mainDark: '#ffbf09',
     /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/fdf7da.png) */
     mainLight: '#fdf7da',
-    /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/7695a3.png) */
-    mainDarkDisabled: '#7695a3',
+    /** 앱 메인 밝은 베이지 색상 ![](https://www.colorhexa.com/f0d381.png) */
+    mainDarkDisabled: '#edc964',
   },
   /** 배경 색상 */
   background: {
--- src/pages/main/ReviewPage.tsx
+++ src/pages/main/ReviewPage.tsx
@@ -1,57 +1,199 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { View, ImageBackground, Text, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native'
+import { format } from 'date-fns'
 import { color, size } from 'common'
 
-function WordContainer({ word, date }) {
+const randomColors = ['#ffbf09', '#8A5E70', '#5BAD90']
+
+const UNKNOWN_WORDS: WordDataType[] = [
+  {
+    id: '0',
+    word: '바나나',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '1',
+    word: '기차',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '2',
+    word: '사과',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '3',
+    word: '할머니',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '4',
+    word: '병원',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '5',
+    word: '강아지',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '6',
+    word: '아빠',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '7',
+    word: '물개',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '8',
+    word: '동물원',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '9',
+    word: '책상',
+    date: new Date(2020, 10, 4),
+  },
+]
+
+const KNOWN_WORDS: WordDataType[] = [
+  {
+    id: '10',
+    word: '대외비',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '11',
+    word: '핍진성',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '12',
+    word: '인플레이션',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '13',
+    word: '기축통화',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '14',
+    word: '배당주',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '15',
+    word: '존버',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '16',
+    word: '치맥',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '17',
+    word: '가화만사성',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '18',
+    word: 'ㅇㅈ',
+    date: new Date(2020, 10, 4),
+  },
+  {
+    id: '19',
+    word: '바다이야기',
+    date: new Date(2020, 10, 4),
+  },
+]
+
+interface WordDataType {
+  id: string
+  word: string
+  date: Date
+}
+
+interface WordContainerProps {
+  wordData: WordDataType
+  index: number
+  onSelected: (word: WordDataType) => void
+  onDeselected: (id: string) => void
+}
+
+function WordContainer({ wordData, index, onSelected, onDeselected }: WordContainerProps) {
+  /** 랜덤한 색상 */
+  const randomColor = randomColors[index % 3]
+  const [isSelected, setIsSelected] = useState(false)
+
   return (
-    <View
+    <TouchableOpacity
       style={{
-        borderColor: color.palette.mainDark,
-        // borderWidth: Math.random() < 0.3 ? 2 : 0,›
+        backgroundColor: isSelected ? randomColor : color.background.white,
         flexDirection: 'row',
-        marginVertical: 4 * size.widthRate,
+        marginVertical: 8 * size.widthRate,
         width: 340 * size.widthRate,
-        height: 40 * size.widthRate,
+        height: 80 * size.widthRate,
         borderRadius: 10 * size.widthRate,
-        backgroundColor: color.background.mainLight,
         paddingHorizontal: 24 * size.widthRate,
         justifyContent: 'space-between',
         alignItems: 'center',
         shadowOpacity: 0.2,
         shadowColor: 'rgb(100, 100, 100)',
-        shadowRadius: 10,
+        shadowRadius: 5,
         shadowOffset: {
-          width: 3,
+          width: 5,
           height: 5,
         },
+      }}
+      onPress={() => {
+        isSelected ? onDeselected(wordData.id) : onSelected(wordData)
+        setIsSelected((prev) => !prev)
       }}>
       <Text
         style={{
-          fontSize: size.normalizeFontSize(14),
-          color: color.text.primary1,
+          fontFamily: 'BMJUA',
+          fontSize: size.normalizeFontSize(32),
+          color: isSelected ? color.background.white : randomColor,
         }}>
-        {word}
+        {wordData.word}
       </Text>
       <Text
         style={{
+          fontFamily: 'BMJUA',
           fontSize: size.normalizeFontSize(14),
-          color: color.text.primary1,
+          color: isSelected ? color.background.white : randomColor,
         }}>
-        {date}
+        {format(wordData.date, 'yyyy.MM.dd')}
       </Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
 export default function ReviewPage() {
+  const [selectedWords, setSelectedWords] = useState<WordDataType[]>([])
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+
+  const onWordSelected = useCallback((word: WordDataType) => {
+    setSelectedWords((data) => data.concat(word))
+  }, [])
+
+  const onWordDeselected = useCallback((id: string) => {
+    setSelectedWords((prevData) => prevData.filter((word) => word.id !== id))
+  }, [])
+
+  useEffect(() => {
+    setButtonDisabled(selectedWords.length < 10)
+  }, [selectedWords.length])
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        backgroundColor: color.background.default,
-        // backgroundColor: 'red',
+        backgroundColor: color.background.white,
       }}>
       <ScrollView
         style={{
@@ -60,43 +202,41 @@ export default function ReviewPage() {
         }}>
         <Text
           style={{
-            fontSize: size.normalizeFontSize(12),
+            fontFamily: 'BMJUA',
+            fontSize: size.normalizeFontSize(24),
             color: color.text.primary1,
             marginLeft: 28 * size.widthRate,
             marginBottom: 12 * size.widthRate,
+            textAlign: 'center',
             // fontWeight: 'bold',
           }}>
-          아직 잘 모르겠어요
+          잘 모르겠어요
         </Text>
-        <WordContainer word={'바나나'} date={'2020.09.24'} />
-        <WordContainer word={'사과'} date={'2020.09.23'} />
-        <WordContainer word={'할머니'} date={'2020.09.23'} />
-        <WordContainer word={'병원'} date={'2020.09.21'} />
-        <WordContainer word={'강아지'} date={'2020.09.21'} />
-        <WordContainer word={'아빠'} date={'2020.09.21'} />
-        <WordContainer word={'물개'} date={'2020.09.18'} />
-        <WordContainer word={'동물원'} date={'2020.09.16'} />
-        <WordContainer word={'책상'} date={'2020.09.16'} />
+        {UNKNOWN_WORDS.map((item, index) => (
+          <WordContainer wordData={item} index={index} onSelected={onWordSelected} onDeselected={onWordDeselected} />
+        ))}
         <Text
           style={{
-            fontSize: size.normalizeFontSize(12),
-            color: color.text.primary1,
+            fontFamily: 'BMJUA',
+            fontSize: size.normalizeFontSize(24),
+            color: color.text.mainDark,
             marginLeft: 28 * size.widthRate,
             marginBottom: 12 * size.widthRate,
             marginTop: 32 * size.widthRate,
+            textAlign: 'center',
             // fontWeight: 'bold',
           }}>
           잘 알아요
         </Text>
-        <WordContainer word={'대외비'} date={'2020.09.24'} />
-        <WordContainer word={'공리주의'} date={'2020.09.23'} />
-        <WordContainer word={'지적재산권'} date={'2020.09.23'} />
-        <WordContainer word={'사력'} date={'2020.09.21'} />
-        <WordContainer word={'강아지'} date={'2020.09.21'} />
-        <WordContainer word={'아빠'} date={'2020.09.21'} />
-        <WordContainer word={'물개'} date={'2020.09.18'} />
-        <WordContainer word={'동물원'} date={'2020.09.16'} />
-        <WordContainer word={'책상'} date={'2020.09.16'} />
+        {KNOWN_WORDS.map((item, index) => (
+          <WordContainer
+            key={item.id}
+            wordData={item}
+            index={index}
+            onSelected={onWordSelected}
+            onDeselected={onWordDeselected}
+          />
+        ))}
         <View style={{ marginBottom: 80 * size.widthRate }} />
       </ScrollView>
       {/* <ImageBackground
@@ -138,9 +278,9 @@ export default function ReviewPage() {
           position: 'absolute',
           bottom: 16 * size.widthRate,
           alignSelf: 'center',
-          borderRadius: 8 * size.widthRate,
+          borderRadius: 16 * size.widthRate,
           marginBottom: 12 * size.heightRate,
-          backgroundColor: color.button.mainDarkDisabled,
+          backgroundColor: buttonDisabled ? color.button.mainDarkDisabled : color.button.mainDark,
           justifyContent: 'center',
           alignItems: 'center',
           shadowOpacity: 0.7,
@@ -151,8 +291,15 @@ export default function ReviewPage() {
             height: 5,
           },
         }}
-        onPress={() => {}}>
-        <Text style={{ fontSize: size.normalizeFontSize(17), fontWeight: 'bold', color: color.text.white }}>
+        onPress={() => {}}
+        disabled={buttonDisabled}>
+        <Text
+          style={{
+            fontFamily: 'BMJUA',
+            fontSize: size.normalizeFontSize(17),
+            fontWeight: 'bold',
+            color: color.text.white,
+          }}>
           복습 하기
         </Text>
       </TouchableOpacity>
