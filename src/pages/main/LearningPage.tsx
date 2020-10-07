060d4be79b99ae0b6fdb0b9a6058a586e7773e9d
SeunghyunWoo <seunghyunwoo99@gmail.com>
2020-10-07 18:20:40 +0900

    오늘의학습: 학습 결과 안내 모달 추가

--- src/pages/main/LearningPage.tsx
+++ src/pages/main/LearningPage.tsx
@@ -58,7 +58,8 @@ export default function LearningPage() {
           pointerDirection="down"
           style={{ position: 'absolute', bottom: -270 * size.widthRate, right: 30 * size.widthRate }}
         /> */}
-        <ReactModal
+        {/* 오늘의 학습 시작 안내 모달 */}
+        {/* <ReactModal
           isVisible={showModal}
           onBackButtonPress={() => setShowModal(false)}
           onBackdropPress={() => setShowModal(false)}>
@@ -158,7 +159,171 @@ export default function LearningPage() {
               </Text>
             </TouchableOpacity>
           </View>
-        </ReactModal>
+        </ReactModal> */}
+        {/* 오늘의 학습 시작 안내 모달 */}
+        {/* <ReactModal
+          isVisible={showModal}
+          onBackButtonPress={() => setShowModal(false)}
+          onBackdropPress={() => setShowModal(false)}>
+          <View
+            style={{
+              backgroundColor: color.background.mainLight,
+              width: 300 * size.widthRate,
+              // height: 180 * size.widthRate,
+              borderRadius: 16 * size.widthRate,
+              shadowOpacity: 0.7,
+              shadowColor: 'rgb(100, 100, 100)',
+              shadowRadius: 10,
+              shadowOffset: {
+                width: 3,
+                height: 5,
+              },
+              padding: 32 * size.widthRate,
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}>
+            <Image
+              source={require('icons/todayLearningMini.png')}
+              style={{
+                width: 40 * size.widthRate,
+                height: 40 * size.widthRate,
+                marginBottom: 12 * size.widthRate,
+                left: 4 * size.widthRate,
+              }}
+            />
+            <Text
+              style={{
+                fontFamily: 'BMJUA',
+                fontSize: size.normalizeFontSize(24),
+                color: color.text.primary1,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                // marginTop: 4 * size.heightRate,
+                marginBottom: 16 * size.heightRate,
+              }}>
+              오늘도 잘 했어요!
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'BMJUA',
+                fontSize: size.normalizeFontSize(18),
+                color: color.text.secondary1,
+                textAlign: 'center',
+                marginBottom: 16 * size.heightRate,
+              }}>
+              {'이진우 어린이, 오늘로'}
+              <Text
+                style={{
+                  fontSize: size.normalizeFontSize(24),
+                  color: color.text.primary1,
+                }}>
+                {' 15 '}
+              </Text>
+              {'번 째\n학습이네요! 대단해요 👏🏻'}
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'BMJUA',
+                fontSize: size.normalizeFontSize(18),
+                color: color.text.secondary1,
+                textAlign: 'center',
+                marginBottom: 16 * size.heightRate,
+              }}>
+              {'오늘은 총'}
+              <Text
+                style={{
+                  fontSize: size.normalizeFontSize(24),
+                  color: color.text.primary1,
+                }}>
+                {' 12 '}
+              </Text>
+              {'분 동안'}
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'BMJUA',
+                fontSize: size.normalizeFontSize(18),
+                color: color.text.secondary1,
+                textAlign: 'center',
+                marginBottom: 16 * size.heightRate,
+              }}>
+              {'낱말'}
+              <Text
+                style={{
+                  fontSize: size.normalizeFontSize(24),
+                  color: color.text.primary1,
+                }}>
+                {' 10 '}
+              </Text>
+              {'개를 배웠어요.'}
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'BMJUA',
+                fontSize: size.normalizeFontSize(18),
+                color: color.text.secondary1,
+                textAlign: 'center',
+                marginBottom: 16 * size.heightRate,
+              }}>
+              {'문제'}
+              <Text
+                style={{
+                  fontSize: size.normalizeFontSize(24),
+                  color: color.text.primary1,
+                }}>
+                {' 10 '}
+              </Text>
+              {'개 중'}
+              <Text
+                style={{
+                  fontSize: size.normalizeFontSize(24),
+                  color: color.text.primary1,
+                }}>
+                {' 5 '}
+              </Text>
+              {'개를 맞았어요.'}
+            </Text>
+            <Text
+              style={{
+                fontFamily: 'BMJUA',
+                fontSize: size.normalizeFontSize(18),
+                color: color.text.secondary1,
+                textAlign: 'center',
+                marginBottom: 16 * size.heightRate,
+              }}>
+              {'내일 또 봐요 😉'}
+            </Text>
+            <TouchableOpacity
+              style={{
+                width: 240 * size.widthRate,
+                height: 42 * size.widthRate,
+                borderRadius: 12 * size.widthRate,
+                marginBottom: 12 * size.heightRate,
+                backgroundColor: color.button.mainDark,
+                justifyContent: 'center',
+                alignItems: 'center',
+                shadowOpacity: 0.5,
+                shadowColor: 'rgb(100, 100, 100)',
+                shadowRadius: 8 * size.widthRate,
+                shadowOffset: {
+                  width: 3,
+                  height: 5,
+                },
+              }}
+              onPress={() => {}}>
+              <Text
+                style={{
+                  fontFamily: 'BMJUA',
+                  fontSize: size.normalizeFontSize(20),
+                  fontWeight: 'bold',
+                  color: color.text.white,
+                }}>
+                네
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ReactModal> */}
       </ImageBackground>
     </View>
   )
