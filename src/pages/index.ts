f1e1db180120098d347c9474a3d495e0b3a698a5
SeunghyunWoo <seunghyunwoo99@gmail.com>
2020-10-08 03:58:19 +0900

    LogInPage 삭제

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,4 @@
 export { default as FrontPage } from './FrontPage'
-export { default as LogInPage } from './LogInPage'
 export { default as MainPage } from './MainPage'
 export { default as SettingPage } from './SettingPage'
 export { default as ProfilePage } from './ProfilePage'
