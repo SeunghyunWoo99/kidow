65d530477ec97e171581ad8c64aba08771dd19c9
SeunghyunWoo <seunghyunwoo99@gmail.com>
2020-10-08 02:40:48 +0900

    BottomButton 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as MainTabBar } from './MainTabBar'
 export { default as MainHeader } from './MainHeader'
 export { default as Tooltip } from './Tooltip'
+export { default as BottomButton } from './BottomButton'
