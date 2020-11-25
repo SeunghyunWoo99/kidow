196e2eebcc54860d914b12f5844b1a75e19933ae
SeunghyunWoo <seunghyunwoo99@gmail.com>
2020-11-25 17:02:38 +0900

    status bar 설정

--- src/navigation/SettingStackNavigator.tsx
+++ src/navigation/SettingStackNavigator.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, StatusBar } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { SettingPage } from 'pages'
@@ -11,55 +11,46 @@ export default function SettingStackNavigator() {
   const navigation = useNavigation()
 
   return (
-    <Stack.Navigator
-      initialRouteName="SettingsPage"
-      screenOptions={{
-        safeAreaInsets: { top: 0 },
-        headerStyle: {
-          height: 56 * size.heightRate,
-        },
-      }}>
-      <Stack.Screen
-        name="SettingPage"
-        component={SettingPage}
-        options={{
-          headerTitle: () => (
-            <Text
-              style={{
-                fontFamily: 'BMJUA',
-                fontSize: size.normalizeFontSize(18),
-                color: color.text.primary1,
-              }}>
-              환경설정
-            </Text>
-          ),
-          headerLeft: () => (
-            <TouchableOpacity onPress={() => navigation.goBack()} style={{ margin: 12 * size.widthRate }}>
+    <>
+      <StatusBar barStyle="light-content" animated={true} />
+      <Stack.Navigator
+        initialRouteName="SettingPage"
+        screenOptions={{
+          safeAreaInsets: { top: 0 },
+          headerStyle: {
+            height: 56 * size.heightRate,
+          },
+        }}>
+        <Stack.Screen
+          name="SettingPage"
+          component={SettingPage}
+          options={{
+            headerTitle: () => (
               <Text
                 style={{
                   fontFamily: 'BMJUA',
-                  fontSize: size.normalizeFontSize(16),
-                  color: color.text.secondary1,
+                  fontSize: size.normalizeFontSize(18),
+                  color: color.text.primary1,
                 }}>
-                취소
+                환경설정
               </Text>
-            </TouchableOpacity>
-          ),
-          headerRight: () => (
-            <TouchableOpacity onPress={() => navigation.goBack()} style={{ margin: 12 * size.widthRate }}>
-              <Text
-                style={{
-                  fontFamily: 'BMJUA',
-                  fontSize: size.normalizeFontSize(16),
-                  color: color.text.mainDark,
-                }}>
-                완료
-              </Text>
-            </TouchableOpacity>
-          ),
-          // headerTransparent: true,
-        }}
-      />
-    </Stack.Navigator>
+            ),
+            headerLeft: () => null,
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginHorizontal: 20 * size.widthRate }}>
+                <Text
+                  style={{
+                    fontFamily: 'BMJUA',
+                    fontSize: size.normalizeFontSize(16),
+                    color: color.text.mainDark,
+                  }}>
+                  완료
+                </Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </>
   )
 }
